554d2ee7516c6d8d5e97aedbfd994c41c249b652
M-Valdy <mevald620@gmail.com>
2026-02-12 12:03:53 -0700

    Added first files to new Git repository

---
+++ model.h
@@ -0,0 +1,16 @@
+typedef struct {
+    Chuck Chuck;
+    WomenWalking WomenWalking;
+    WomenSwimming WomenSwimming;
+    Tree Tree;
+    Water Water;
+    Shelf Shelf;
+    Meat Meat;
+    Bread Bread;
+    Fruit Fruit;
+    Cashier Cashier;
+    Door Door;
+    Timer Timer;
+    Vampire Vampire;
+    Bats Bats;
+} Model;
