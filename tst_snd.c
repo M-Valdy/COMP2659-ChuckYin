98f9f035c7a0d1f61dd28535bfcb21cebcbb5082
M-Valdy <mevald620@gmail.com>
2026-03-25 11:30:19 -0600

    Improved the test drive

--- tst_snd.c
+++ tst_snd.c
@@ -3,17 +3,20 @@
 #include <osbind.h>
 
 void psg_tst () {
+    read_psg(0);
     set_tone(0, 248);         /* channel A */
     read_psg(0);
+    read_psg(7);
     enable_channel(0, 1, 0);  /* tone enabled, noise disabled */
-    read_psg(0);
+    read_psg(7);
+    read_psg(8);
     set_volume(0, 11);        /* volume = 11 */
-    read_psg(0);
+    read_psg(8);
 
     while (!Cconis())
         ;
 
-    set_volume(0, 0);         /* stop sound */
+    stop_sound();         /* stop sound */
     read_psg(0);
     Cnecin();                 /* consume key */
 
